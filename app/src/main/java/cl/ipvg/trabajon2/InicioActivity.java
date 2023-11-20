ca26ec4316a53da0f539141be6ebaa40a51e7f64
PaulinaSeminario <paulina.seminario@virginiogomez.cl>
2023-11-20 01:46:30 -0300

    app 20-11-2023

--- app/src/main/java/cl/ipvg/trabajon2/InicioActivity.java
+++ app/src/main/java/cl/ipvg/trabajon2/InicioActivity.java
@@ -2,10 +2,22 @@ package cl.ipvg.trabajon2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,11 +25,31 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 
 public class InicioActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
 
     EditText txtlatitud, txtlongitud;
     GoogleMap mMap;
+    Spinner spin_text;
+    Button btn_buscar;
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    double currentLat = 0, currentLong = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +60,89 @@ public class InicioActivity extends AppCompatActivity implements OnMapReadyCallb
         txtlongitud = findViewById(R.id.txtlongitud);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
         mapFragment.getMapAsync(this);
+
+        spin_text = findViewById(R.id.spintexto);
+        btn_buscar = findViewById(R.id.btnBuscar);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
+
+        String[] placeTypeList = {"parques"};
+        String[] placeNameList = {"Parques"};
+
+        spin_text.setAdapter(new ArrayAdapter<>(InicioActivity.this, android.R.layout.simple_spinner_dropdown_item, placeNameList));
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(InicioActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            getCurrentLocation();
+
+        }else {
+            ActivityCompat.requestPermissions(InicioActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+        btn_buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i = spin_text.getSelectedItemPosition();
+
+                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +
+                        "?location=" + currentLat + "," + currentLong +
+                        "&radius=5000" +
+                        "&types=" +
+                        placeTypeList[i] +
+                        "&sensor= true"
+                        + "&key="
+                        + getResources().getString(R.string.google_map_key);
+
+
+                new PlaceTask().execute(url);
+            }
+        });
+
+    }
+
+    private void getCurrentLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    currentLat = location.getLatitude();
+
+                    currentLong = location.getLongitude();
+
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            mMap = googleMap;
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(currentLat, currentLong), 10
+                            ));
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }
+        }
     }
 
     @Override
@@ -36,9 +151,6 @@ public class InicioActivity extends AppCompatActivity implements OnMapReadyCallb
         this.mMap.setOnMapClickListener(this);
         this.mMap.setOnMapLongClickListener(this);
 
-        LatLng chile = new LatLng(-33.4717787,-70.9594905);
-        mMap.addMarker(new MarkerOptions().position(chile).title("Chile"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(chile));
     }
 
     @Override
@@ -46,9 +158,6 @@ public class InicioActivity extends AppCompatActivity implements OnMapReadyCallb
         txtlatitud.setText(""+latLng.latitude);
         txtlongitud.setText(""+latLng.longitude);
         mMap.clear();
-        LatLng chile = new LatLng(latLng.latitude,latLng.longitude);
-        mMap.addMarker(new MarkerOptions().position(chile).title(""));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(chile));
     }
 
     @Override
@@ -56,9 +165,76 @@ public class InicioActivity extends AppCompatActivity implements OnMapReadyCallb
         txtlatitud.setText(""+latLng.latitude);
         txtlongitud.setText(""+latLng.longitude);
         mMap.clear();
-        LatLng chile = new LatLng(latLng.latitude,latLng.longitude);
-        mMap.addMarker(new MarkerOptions().position(chile).title(""));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(chile));
+    }
 
+    private class PlaceTask extends AsyncTask<String,Integer,String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String data = null;
+            try {
+                data = downloadUrl(strings[0]);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            new  ParserTask().execute(s);
+        }
+    }
+
+    private String downloadUrl(String string) throws IOException {
+
+        URL url = new URL(string);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+        InputStream stream = connection.getInputStream();
+        BufferedReader reader  = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+        String line = "";
+        while ((line = reader.readLine()) !=null){
+            builder.append(line);
+        }
+        String data = builder.toString();
+        reader.close();
+        return data;
+    }
+
+    private class ParserTask extends AsyncTask<String,Integer, List<HashMap<String,String>>> {
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... strings) {
+            JsonParser jsonParser = new JsonParser();
+            List<HashMap<String,String>> mapList = null;
+            JSONObject object = null;
+            try {
+                object = new JSONObject(strings[0]);
+                mapList = jsonParser.parseResult(object);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+            return mapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+            mMap.clear();
+            for (int i=0; i<hashMaps.size();i++){
+                HashMap<String,String> hashMapList = hashMaps.get(i);
+                double lat = Double.parseDouble(hashMapList.get("lat"));
+                double lng = Double.parseDouble(hashMapList.get("lng"));
+                String name = hashMapList.get("name");
+                LatLng latLng = new LatLng(lat, lng);
+                MarkerOptions options = new MarkerOptions();
+                options.position(latLng);
+                options.title(name);
+                mMap.addMarker(options);
+
+            }
+        }
     }
 }
